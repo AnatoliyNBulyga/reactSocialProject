8c4749bff7150ac6707370d4e5e1489a3bf218d3
Anatoliy <anatoliynbulyga@gmail.com>
2019-11-30 15:30:23 +0200

    lesson 59 add ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from '../../common/Preloader/Preloader'
 
 const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
     return (
         <div className={s.description}>
-            <img src="https://i.dailymail.co.uk/1s/2019/09/16/12/18530818-7468587-image-a-23_1568631983753.jpg" alt="ava"/>
-            <div className={s.description_body}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis remn umquam voluptatem accusantium, vel quos labore, natus eius alias ut nesciunt ullam ad, in doloribus quo placeat cumque ex itaque.</div>
+            <img src={props.profile.photos.small} alt={props.profile.fullName}/>
+            <div className={s.description_body}>{props.profile.aboutMe}</div>
         </div>
     );
 }
