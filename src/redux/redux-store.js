179874bfe37669f8adaf50824dcbf864859c39fe
Anatoliy <anatoliynbulyga@gmail.com>
2019-12-03 22:30:14 +0200

    lesson 60 login  auth/me

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import sidebarReducer from "./sidebarReducer";
 import profileReducer from "./profileReducer";
 import dialogReducer from "./dialogReducer";
 import usersReducer from "./UsersReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
     sidebar: sidebarReducer,
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 let store = createStore(reducers);
 
