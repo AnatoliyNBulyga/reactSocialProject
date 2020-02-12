443f55317687d7c5b9955f4fbd2a77caa8b72954
Anatoliy <anatoliynbulyga@gmail.com>
2020-02-12 21:30:42 +0200

    Updates

---
+++ precache-manifest.58442cbadc62da5781b12c690ff59890.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc4594739b5b0a2025b868c2b0a1f00",
+    "url": "/reactSocialProject/index.html"
+  },
+  {
+    "revision": "9e6ddad61b7a45e37d93",
+    "url": "/reactSocialProject/static/css/3.7989067e.chunk.css"
+  },
+  {
+    "revision": "3e833e859e34873ac5b4",
+    "url": "/reactSocialProject/static/css/4.bd9584b2.chunk.css"
+  },
+  {
+    "revision": "d1396e6d78a0cf29632c",
+    "url": "/reactSocialProject/static/css/main.3c3ecde1.chunk.css"
+  },
+  {
+    "revision": "5fa2bdde5b6fdfaf43da",
+    "url": "/reactSocialProject/static/js/2.8beab866.chunk.js"
+  },
+  {
+    "revision": "9e6ddad61b7a45e37d93",
+    "url": "/reactSocialProject/static/js/3.41b3c7c2.chunk.js"
+  },
+  {
+    "revision": "3e833e859e34873ac5b4",
+    "url": "/reactSocialProject/static/js/4.b2a2f4f9.chunk.js"
+  },
+  {
+    "revision": "d1396e6d78a0cf29632c",
+    "url": "/reactSocialProject/static/js/main.8a4ed9d3.chunk.js"
+  },
+  {
+    "revision": "4fdce0c8f7422a20da02",
+    "url": "/reactSocialProject/static/js/runtime-main.15250ef6.js"
+  },
+  {
+    "revision": "1816dc70e63024e24f9747b3d0f78869",
+    "url": "/reactSocialProject/static/media/loader.1816dc70.svg"
+  },
+  {
+    "revision": "e4c2ea96ad1ec2661aef6b390dc33d81",
+    "url": "/reactSocialProject/static/media/user1.e4c2ea96.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e1b8e3fb5bf892280ba78beca68ee2d.js"
+  "/reactSocialProject/precache-manifest.58442cbadc62da5781b12c690ff59890.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactSocialProject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
